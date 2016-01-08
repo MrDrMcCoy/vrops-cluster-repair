6fd398d2de05864c8654dad24a5e8cf55de4333f
mccoyj <mccoyj@vmware.com>
2016-01-08 02:16:35 -0700

    New Features! Better Instructions! - Added interactive prompt to correct node list - Added interactive prompt for the activity cleaner - Added interactive prompt for disabling IPv6 - Added interactive prompt for pak file removal - Fixed pak file remover to be compatible with 6.0-6.1 - Moved time drift and latency checkers to beginning of script so you don't have to run the whole thing every time - Cleaned formatting and expanded upon the interactive instructions. Should be easier to read and understand

--- README.md
+++ README.md
@@ -1,29 +1,46 @@
-# vrops-cluster-repair
+# vROps Cluster Repair
 A script that helps return the cluster to a healthy state.
 
 ## Things it does:
 
-* Optionally clears out the alarms/alerts DB. Useful if a message is stuck in there.
-* Archives malformed upgrade state files, which new updates will try to parse before they run. http://kb.vmware.com/kb/2120616
-* Archives previous updates, patches and add-on Management Packs, which may cause issues if they still linger in your update queue.
-* Archives large database blobs from a previous data migrations, which may choke the update process. http://kb.vmware.com/kb/2132479
-* Alters the cluster state files to remove malformed data in them and set the cluster to a clean offline state. 
-* Helps you check for time drift between the nodes, which is bad.
-* Helps you check for latency issues between the nodes.
-* It also has a disabled feature to clear out activities, which you can use in situations where a heavier hand is needed.
+- Cleanly shuts down all cluster services, which is good if the Admin UI is unresponsive.
+- Backs up and removes malformed upgrade state files. You don't need them, and new updates may fail to parse them properly when they run. http://kb.vmware.com/kb/2120616
+- Backs up and removes previous update, patch, and Management Pack files -- which can cause issues with future upgrades and cluster expansions if they remain.
+- Backs up and removes large database blobs that may be lingering from a previous data migration, which can break future updates. http://kb.vmware.com/kb/2132479
+- Backs up and alters the cluster state files to remove malformed data and set the cluster to a clean offline state. 
+- Helps you check for time drift between the nodes, which is bad.
+- Helps you check for latency issues between the nodes.
+- Optionally backs up and clears out the alarms/alerts DB. Useful if a message is stuck in there.
+- Optionally backs up and clears out the activities queue, which may be necessary if the script does not fix everything on the first go.
+- Optionally disables IPv6 on your nodes, which is a good idea if you do not use IPv6 in your environment. 
+
+## Things it does not do:
+
+- Check for free disk space (planned for future release)
+- Tell you if you are undersized (Planned for future release)
+- Fix database corruption (may happen in future release)
+- Identify miscellaneous configuration issues (may happen in future release)
+- Make breakfast (I mean, I suppose it *could*... but you really shouldn't put food on your CPU heatsink)
 
 ## Instructions for use:
 
-* Enable SSH on all your nodes.
-  * Open a VM console session to the node.
-  * Press ALT+F1 to get to a console session.
-  * Log in as root.
-    * If you have never logged in to the console before, the password is blank, and it will prompt you to change it.
-  * Start the SSH service with `service sshd start`.
-    * To make this change permanent, you can also type `chkconfig sshd on`.
-* Transfer the script to the Master Node via your preferred SCP/SFTP client (such as WinSCP, Filezilla, etc)
-* Connect to the master node via your preferred SSH client (such as Putty or MobaXterm).
-* Execute the script by typing `bash vrops-cluster-repair.bash` and follow the prompts.
-  * If it does not detect all the nodes in your cluster, you should edit the file with `vim` and add them manually. There are comments in the file to guide you.
+- Enable SSH on all your nodes.
+  - Open a VM console session to the node.
+  - Press ALT+F1 to get to a console session.
+  - Log in as root.
+    - If you have never logged in to the console before, the password is blank, and it will prompt you to change it.
+  - Start the SSH service with `service sshd start`.
+    - To make this change permanent, you can also type `chkconfig sshd on`.
+- Transfer the script to the Master Node via your preferred SCP/SFTP client (such as WinSCP, Filezilla, etc)
+  - A good place to put the file is /tmp/
+  - You can also paste the text of the script into a new file using SSH and `vim`.
+- Connect to the master node via your preferred SSH client (such as Putty or MobaXterm).
+- Execute the script by typing `bash vrops-cluster-repair.bash`.
+- Follow the prompts. The script is interactive and imbued with magic beard powers, let it be your guide.
+- Verify that the UI comes up and that you are able to log in. It may take some time after the cluster has started.
+  - If it comes up, rejoice!
+  - If it errors out, [get mad](https://youtu.be/g8ufRnf2Exc)! ...then [file an issue](https://github.com/nakedhitman/vrops-cluster-repair/issues).
+- ???
+- Profit.
 
-Please submit ideas for improving this script under Issues.
+Please submit ideas for improving this script under [Issues](https://github.com/nakedhitman/vrops-cluster-repair/issues), tagged with [Feature Request] in the subject line.
--- vrops-cluster-repair.bash
+++ vrops-cluster-repair.bash
@@ -1,63 +1,166 @@
 #!/bin/bash
-#Version 0.6.7
-trap exit SIGINT SIGKILL SIGTERM
-log=vrops-cluster-repair.log
-read -p "This script will reset your vROps cluster to a known good state. 
-It is not supported by VMware, and you should take a snapshot before running it.
-Please note that SSH must be enabled on _ALL_ nodes for this to work, which is not the default.
+#Version 0.6.8
+log="/tmp/vrops-cluster-repair.log"
+trap "(echo -ne '\n==========| Exiting per user signal at ' ; date | tr -d '\n' ; echo ' |==========') | tee -a $log; exit" SIGINT SIGKILL SIGTERM
+rundate="`date +%F_%I.%M.%S_%p_%Z`"
+echo -e "\n==========| Starting repair script at $rundate |==========\n" >> $log
+detected_nodes="`grep -oP '"ip_address":"[^"]+"' $STORAGE/db/casa/webapp/hsqldb/casa.db.script | cut -d '"' -f4 | tr '\n' ' '`"
+if [ "$detected_nodes" = "" ]; then detected_nodes="localhost" ; fi 2>&1 | tee -a $log
 
-Press enter to continue or CTRL-C to exit. # " nothing
-
-echo -e "==========| Starting repair script at `date` |==========\n" >> $log
-exec > >(tee -a $log );exec 2> >(tee -a $log >&2)
-detected_nodes="$(grep -oiP '"ip_address":"[a-zA-Z0-9\.-_]+"' $STORAGE_DB_VCOPS/../casa/webapp/hsqldb/casa.db.script | cut -d '"' -f4 | tr '\n' ' ')"
-if [ "$detected_nodes" = "" ]; then detected_nodes="localhost" ; fi
-
-# You may enter a space-separated list of FQDNs or IPs for each node in the cluster in place of $detected_nodes between the quotes below.
-# Use the following order for your nodes where applicable: Master, Master Replica, Data, Remote Collector
+#########
+# You may enter a space-separated list of FQDNs or IPs for each node in the 
+# cluster. Do this in place of $detected_nodes between the quotes below.
+# Use the following order for your nodes where applicable: 
+# Master, Master Replica, Data, Remote Collector
 node_list="$detected_nodes"
-stop_order="$(echo "$node_list" | tr ' ' '\n' | tac | tr '\n' ' ')"
-read -p "Please verify that this is a complete list of all your nodes:
+#########
 
-$node_list
+read -p "
+#####
+This script will reset your vROps cluster to a known good state. 
+It is not supported by VMware, and you should take a snapshot before running
+it. Please note that SSH must be enabled on _ALL_ nodes for this to work, which
+is not the default. Press enter to continue or CTRL+C to exit. 
+#####
+\$ "
 
-If not, please edit this script to ensure that all cluster members are manually added.
+read -p "
+#####
+Please verify that this is a complete list of all your nodes:
 
-Press enter to continue or CTRL-C to exit. # " nothing
+[ $node_list ]
 
-echo -e "\nChecking SSH connectivity to nodes..."
-	disconnect () { echo "Unable to connect to $1 on port 22, please ensure ssh is enabled on this host. Exiting."; exit 1; }
-	for h in $node_list; do 
-		timeout -k 3 2 echo "Port 22 is open on $h" 3> /dev/tcp/$h/22 || disconnect $h; 
-	done
+If not, you may enter a space-separated list of IPs/FQDNs at the prompt, or edit
+this script to ensure that all cluster members are manually added. You may also
+leave the prompt blank and press enter if the above list is correct, or press 
+CTRL+C to exit.
+#####
+\$ "
 
-if [ ! -e "$HOME/.ssh/id_rsa.pub" ]; then 
-	echo -e "\nNo SSH keys found. We will generate one now. Please accept all defaults."
-		ssh-keygen -q || exit 1
+if [ "$override" != "" ]; then
+	node_list=$override
+	read -p "
+#####
+The node list is now changed to the following:
+
+[ $node_list ]
+
+Press enter to continue or CTRL+C to exit
+#####
+\$ "
 fi
 
-echo -e "\nWe will now push the SSH host-key out to the nodes. This will require logging in to each of the nodes one time each. Please follow the prompts as they appear and accept all defaults.\n"
+stop_order="`echo "$node_list" | tr ' ' '\n' | tac | tr '\n' ' '`"
+
+echo -e "\nChecking SSH connectivity to nodes..." 2>&1 | tee -a $log
+	disconnect () { echo -e "Unable to connect to $1 on port 22, please ensure ssh is enabled on this host.\nWill now exit." 2>&1 | tee -a $log; exit 1; }
+	for h in $node_list; do 
+		timeout -k 3 2 echo "Port 22 is open on $h" 3> /dev/tcp/$h/22 || disconnect $h; 
+	done 2>&1 | tee -a $log
+
+if [ ! -e "$HOME/.ssh/id_rsa.pub" ]; then 
+	echo "
+##### 
+No SSH key found. We will generate one now. Please accept all defaults.
+#####
+" 2>&1 | tee -a $log
+		ssh-keygen -q || exit 1
+fi 2>&1 | tee -a $log
+
+echo "
+#####
+We will now push the SSH host-key out to the nodes. This will require logging
+in to each of the nodes one time each.
+#####
+" 2>&1 | tee -a $log
 for h in $node_list; do 
 	ssh-copy-id -o StrictHostKeyChecking=no root@$h || exit 1
-done
+done 2>&1 | tee -a $log
 
-read -p "Clear out alarms and alerts from database? (optional, requires cluster to be online)
-(y/N) # " yn
+echo -e "\n==========| Getting date/time of each node |==========" 2>&1 | tee -a $log
+for h in $node_list; do
+	echo "Date/time on $h is `ssh -q root@$h date`" &
+done 2>&1 | tee -a $log
+wait
+read -p "
+#####
+If any of the node clocks are more than 2 seconds out of sync, it can cause 
+issues with the cluster services. Please ensure that they are obtaining a good 
+timestamp either from the host (via Tools) or from NTP as per the documentation.
+Press enter to continue.
+#####
+\$ "
+
+echo -e "\n==========| Getting latency between nodes |==========" 2>&1 | tee -a $log
+for h in $node_list; do
+	echo "Average round trip latency to $h is `ping -c 10 -i 0.2 -W 5 $h | grep rtt | cut -f5 -d '/'` miliseconds" &
+done  2>&1 | tee -a $log
+wait
+read -p "
+#####
+Core nodes (Master, Master Replica, Data) should not have a latency higher than
+1 milisecond. Remote collectors should not have a latency higher than 100
+miliseconds. It's recommended that core nodes be kept on the same physical LAN.
+Press enter to continue.
+#####
+\$ "
+
+read -p "
+#####
+Would you like to clear out alarms and alerts from database? This can improve
+performance. Ongoing issues will have new alerts generated. It will fail if
+the cluster is not online.
+(Optional. Default: NO)
+#####
+(yes/NO) \$ " yn
+
 if [[ "$yn" =~ y|yes|1 ]]; then 
 		echo "This will produce errors on remote collectors, please ignore them."
 	for h in $node_list; do 
 		echo -e "\nBacking up alerts and alarms database on $h..."
-			ssh -q root@$h "sudo -u postgres /opt/vmware/vpostgres/current/bin/pg_dumpall | gzip -9 > \$STORAGE/db/vcops/vpostgres/pg_dumpall-`date +%F_%I.%M.%S_%p_%Z`.gz"
+			ssh -q root@$h "sudo -u postgres /opt/vmware/vpostgres/current/bin/pg_dumpall | gzip -9 > \$STORAGE/db/vcops/vpostgres/pg_dumpall-$rundate.gz"
 		echo -e "\nTruncating alerts and alarms on $h..."
 			ssh -q root@$h "sudo -u postgres /opt/vmware/vpostgres/current/bin/psql vcopsdb -c 'truncate alarm cascade;'"
 			ssh -q root@$h "sudo -u postgres /opt/vmware/vpostgres/current/bin/psql vcopsdb -c 'truncate alert cascade;'"
 	done
-fi
+fi 2>&1 | tee -a $log
 
-echo -e "\nWe will now perform the cluster repair. You may see some messages about services not stopping properly. This is expected.\n"
-read -p "Press enter to continue or CTRL-C to exit. # " nothing
+read -p "
+#####
+Would you like to disable IPv6? This is recommended if you do not use IPv6.
+(Optional. Default: NO)
+#####
+(yes/NO) \$ " ipv6
+
+read -p "
+#####
+Would you like to clear out the activity cache? This should only be necessary
+if running this script without does not restore all functionality as desired.
+(Optional. Default: NO) 
+#####
+(yes/NO) \$ " activities
+
+read -p "
+#####
+Would you like to archive inatalled PAK files? They are left behind by
+upgrades, patches, and add-ons. They can interfere with future upgrades and
+cluster expansions. This is safe and recommended, with the caveat that the 
+cluster status page will no longer show any management packs as installed,
+even though they are. The Solutions page will still show them properly.
+(Optional. Default: YES)
+#####
+(YES/no) \$ " pak
+
+read -p "
+#####
+We will now perform the cluster repair. You may see some messages about services
+not stopping properly. This is safe to ignore. Press enter to continue or
+CTRL-C to exit.
+#####
+\$ "
+
 for h in $stop_order; do
-	echo -e "\n==========| Performing cluster state repair on $h |=========="
+	echo -e "\n==========| Performing actions on $h |=========="
 	echo "Taking node offline..."
 		ssh -q root@$h "\$VMWARE_PYTHON_BIN \$VCOPS_BASE/../vmware-vcopssuite/utilities/sliceConfiguration/bin/vcopsConfigureRoles.py --action bringSliceOffline --offlineReason \"repairing\""
 	echo "Stopping services..."
@@ -66,45 +169,52 @@ for h in $stop_order; do
 		ssh -q root@$h "service vmware-vcops-watchdog stop"
 		ssh -q root@$h "service vmware-casa stop"
 	echo "Altering cluster state..."
-		ssh -q root@$h "sed -ri.bak 's/sliceonline = \w+/sliceonline = false/g;s/failuredetected = \w+/failuredetected = false/g;s/offlinereason = \w+/offlinereason = /g' \$VCOPS_BASE/../vmware-vcopssuite/utilities/sliceConfiguration/data/roleState.properties"
-		ssh -q root@$h "sed -ri.bak 's/\"onlineState\":\"\w+\"/\"onlineState\":\"OFFLINE\"/g;s/\"ha_transition_state\":\"\w+\"/\"ha_transition_state\":\"NONE\"/g;s/\"initialization_state\":\"\w+\"/\"initialization_state\":\"NONE\"/g;s/\"online_state\":\"\w+\"/\"online_state\":\"OFFLINE\"/g;s/\"online_state_reason\":\"[a-zA-Z0-9:,_\?\-\.]+\"/\"online_state_reason\":\"\"/g' \$STORAGE_DB_VCOPS/../casa/webapp/hsqldb/casa.db.script"
-	#This should only be done as a last resort.
-	#echo "Backing up and removing activities..."
-	#	ssh -q root@$h "zip -r2mq \$STORAGE/db/vcops/activity-backup-`date +%F_%I.%M.%S_%p_%Z`.zip \$STORAGE/db/vcops/activity/*" 
-	echo "Moving old upgrade pak files..."
-		ssh -q root@$h "mkdir -p \$STORAGE/db/casa/pak/dist_pak_files_backup"
-		ssh -q root@$h "mv \$STORAGE/db/casa/pak/dist_pak_files/* \$STORAGE/db/casa/pak/dist_pak_files_backup/"
+		ssh -q root@$h "perl -pi_$rundate.bak -e 's/sliceonline = .+/sliceonline = false/g;s/failuredetected = .+/failuredetected = false/g;s/offlinereason = .+/offlinereason = /g' \$VCOPS_BASE/../vmware-vcopssuite/utilities/sliceConfiguration/data/roleState.properties"
+		ssh -q root@$h "perl -pi_$rundate.bak -e 's/\"onlineState\":\"[^\"]+\"/\"onlineState\":\"OFFLINE\"/g;s/\"ha_transition_state\":\"[^\"]+\"/\"ha_transition_state\":\"NONE\"/g;s/\"initialization_state\":\"[^\"]+\"/\"initialization_state\":\"NONE\"/g;s/\"online_state\":\"[^\"]+\"/\"online_state\":\"OFFLINE\"/g;s/\"online_state_reason\":\"[^\"]+\"/\"online_state_reason\":\"\"/g' \$STORAGE/db/casa/webapp/hsqldb/casa.db.script"
 	echo "Backing up and removing old upgrade state file..."
-		ssh -q root@$h "zip -2mq vcops_upgrade_state-`date +%F_%I.%M.%S_%p_%Z`.zip \$STORAGE/log/var/log/vmware/vcops/vcops_upgrade_state.json"
+		ssh -q root@$h "zip -2mq vcops_upgrade_state-$rundate.zip \$STORAGE/log/var/log/vmware/vcops/vcops_upgrade_state.json"
 	echo "Backing up and truncating migration blobs that break upgrades..."
-		ssh -q root@$h "find \$STORAGE_DB_VCOPS/blob/migrationUpgrade/ -type f -size +31M -exec zip -2q \$STORAGE/db/xdb-migrationupgrade-{}-`date +%F_%I.%M.%S_%p_%Z`.zip {} \; -exec truncate -cs 0 {} \;"
+		ssh -q root@$h "find \$STORAGE_DB_VCOPS/blob/migrationUpgrade/ -type f -size +31M -exec zip -2q \$STORAGE/db/xdb-migrationupgrade-{}-$rundate.zip {} \; -exec truncate -cs 0 {} \;"
+	if [[ ! "$pak" =~ n|no ]]; then
+	echo "Backing up and removing old upgrade pak files..."
+		ssh -q root@$h "find \$STORAGE/db/casa/pak/dist_pak_files/ \( -type f -o -type l \) -exec zip -0myq \$STORAGE/db/casa/pak/dist_pak_files_backup_$rundate.zip {} \;"
+	fi
+	if [[ "$activities" =~ y|yes ]]; then
+		echo "Backing up and removing activities..."
+			ssh -q root@$h "zip -r2mq \$STORAGE/db/vcops/activity-backup-$rundate.zip \$STORAGE/db/vcops/activity/*" 
+	fi
+	if [[ "$ipv6" =~ y|yes ]]; then
+		echo "Disabling IPv6..."
+			ssh -q root@$h "echo -e 'alias net-pf-10 off\nalias ipv6 off' >> /etc/modprobe.conf.local ; echo -e 'net.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1\nnet.ipv6.conf.lo.disable_ipv6 = 1' >>/etc/sysctl.conf ; sysctl -p > /dev/null"
+	fi		
+done 2>&1 | tee -a $log
+echo
+
+read -p "
+#####
+From here, it is recommended that you reboot each node, then bring the cluster 
+online from the Admin UI. This will ensure that some additional cached data is 
+cleared, and free up file/port locks. However, if you run in to errors, it may
+be necessary to start the nodes individually. This will result in a false 
+'Offline' state to be reported as the cluster status, but vROps should
+otherwise function normally. If you would like to do this now, please type
+'yes' at the prompt, or simply press enter to exit.
+#####
+(yes/NO) \$ " yn
+
+if [[ "$yn" =~ y|yes|1 ]]; then
+	echo -e "\nBringing cluster back online. You may see some messages about services refusing\nto start due to the cluster state being offline. This can be safely ignored.\n"
+	for h in $node_list; do
+		echo -e "\n==========| Restarting services on $h |=========="
+			ssh -q root@$h "service vmware-casa start"
+			ssh -q root@$h "service vmware-vcops-watchdog start"
+			#ssh -q root@$h "service vmware-vcops start"
+			ssh -q root@$h "service vmware-vcops-web start"
+	done
+	for h in $node_list; do
+		echo -e "\n==========| Bringing node $h online |=========="
+		ssh -q root@$h "\$VMWARE_PYTHON_BIN \$VCOPS_BASE/../vmware-vcopssuite/utilities/sliceConfiguration/bin/vcopsConfigureRoles.py --action bringSliceOnline"
 done
+fi 2>&1 | tee -a $log
 
-echo -e "\nBringing cluster back online. You will see some messages about services refusing to start due to the cluster state being offline. This is expected.\n"
-for h in $node_list; do
-	echo -e "\n==========| Restarting services on $h |=========="
-		ssh -q root@$h "service vmware-casa start"
-		ssh -q root@$h "service vmware-vcops-watchdog start"
-		ssh -q root@$h "service vmware-vcops start"
-		ssh -q root@$h "service vmware-vcops-web start"
-done
-for h in $node_list; do
-	echo -e "\n==========| Bringing node $h online |=========="
-	ssh -q root@$h "\$VMWARE_PYTHON_BIN \$VCOPS_BASE/../vmware-vcopssuite/utilities/sliceConfiguration/bin/vcopsConfigureRoles.py --action bringSliceOnline"
-done 
-
-echo -e "\n==========| Getting date/time of each node |=========="
-for h in $node_list; do
-	echo "Date/time on $h is `ssh -q root@$h date`" &
-done 
-wait
-echo -e "\nIf any of the above times are more than 10 seconds out of sync, that can cause issues in the cluster services. Ensure they are using proper host or NTP time sync or set their clocks manually.\n"
-
-echo -e "\n==========| Getting latency of each node |=========="
-for h in $node_list; do
-	echo "Average round trip latency to $h is `ping -c 10 -i 0.2 -W 5 $h | grep rtt | cut -f5 -d '/'` miliseconds" &
-done 
-wait
-echo -e "\nIf any of the above times are greater than 1 milisecond (except for remote collectors), it can cause database communication issues. Ensure that your nodes are on the same physical LAN and that no other networking issues are in play. Non-remote-collector nodes cannot be separated by a WAN.\n"
-
-echo -e "\n==========| Finished repair script at `date` |==========\n" | tee -a $log
+echo -e "\n==========| Done at `date` |==========\n" 2>&1 | tee -a $log
